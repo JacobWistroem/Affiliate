c380e97cf7b2a0c78b31dcd7665e7655d137d21c
TwizzX <Jacob.wistroem@gmail.com>
2019-10-14 15:51:07 +0200

    Configured skeleton for routing

--- src/index.js
+++ src/index.js
@@ -2,12 +2,20 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { Route, BrowserRouter as Router, Switch } from 'react-router-dom';
 import './Style.css'; //The style
-import App from './App'; 
-import * as serviceWorker from './serviceWorker';
+import Home from './components/Home';
+import Browse from './components/Browse';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const routing = (
+    <Router>
+        <div>
+            <Switch>
+                <Route exact path="/" component={Home} />
+                <Route path="/Browse" component={Browse} />
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+                <Route component={NotFound} />
+            </Switch>
+        </div>
+    </Router>
+)
+
+ReactDOM.render(routing, document.getElementById('root'));
